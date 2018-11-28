deb41977d2a1ce9570b22c93d31b2f0af3ecc266
CORP\MAREPA <matylda.repa@gmail.com>
2018-11-28 18:00:04 +0100

    added new class - bird

---
+++ bird.py
@@ -0,0 +1,33 @@
+from animal import Animal
+
+
+class Bird(Animal):
+
+    def __init__(self):
+        super(Animal, self).__init__() #moge zainicjalizowac klase nie dziedziczac jej , poza jakakolwiek klasa np w jakiejs funkcji che wywol;ac sobie np klase animal-> super
+        #lub:
+        Animal.__init__(self)
+        self.dlugosc_skrzydla = 0
+        self.kolor_upierzenia = ''
+        self.plec = ''
+
+    def get_dlugosc_skrzydla(self):
+        return self.dlugosc_skrzydla
+
+    def get_kolor_upierzenia(self):
+        return self.kolor_upierzenia
+
+    def get_plec(self):
+        return self.plec
+
+    def set_dlugosc_skrzydla(self):
+        return self.dlugosc_skrzydla
+
+    def set_kolor_upierzenia(self):
+        return self.kolor_upierzenia
+
+    def set_plec(self):
+        return self.plec
+
+bird  = Bird()
+bird.get_waga()
